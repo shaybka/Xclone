a0dc01a13f038c49ef4824ebc5825630ccd827c8
aden mohamoud <154498894+shaybka@users.noreply.github.com>
2025-08-13 10:57:58 +0300

    Fix error handling middleware to include 'next' parameter

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.use("/api/users", userRoutes)
 app.use("/api/post", postRoutes)
 
 // Global error handling middleware
-app.use((err, req, res) => {
+app.use((err, req, res,next) => {
   console.error("unhandled error:", err);
   res.status(500).json({
     error: err.message || "Internal Server Error"
